f0e452da173c237a57b414ce15b74a4cba1030b5
Alexandr Rodionov <alexrodionby@gmail.com>
2023-12-25 11:14:52 +0300

    added several reused views, such as AvatarCircleView, Like Heart View, Search Bar View, a new extension for the View, several minor code fixes throughout the project, progress in the home screen

--- MovieBar/Screens/Home/HomeView.swift
+++ MovieBar/Screens/Home/HomeView.swift
@@ -8,17 +8,67 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @ObservedObject var homeVM: HomeViewModel
+    var profileName: String = "Guest"
+    
     var body: some View {
         NavigationView {
             ZStack {
                 Color.custom.primaryDark
                     .ignoresSafeArea()
-                Text("HomeView")
+                VStack(spacing: 16) {
+                    
+                    AvatarLine(profileName: profileName)
+                    
+                    SearchBarView() { searchText in
+                        print("Текст в родителе", searchText)
+                    }
+         
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+            }
+            .onTapGesture {
+                hideKeyboard()
             }
         }
     }
 }
 
 #Preview {
-    HomeView()
+    HomeView(homeVM: HomeViewModel())
+}
+
+extension HomeView {
+    
+    // MARK: - AvatarLine View
+    
+    struct AvatarLine: View {
+        
+        var profileName: String = ""
+        var helloText = LocalizedStringKey("Hello, ")
+        
+        var body: some View {
+            HStack(spacing: 20) {
+                AvatarCircleView(avatarFrameSize: 40, imageFrameSize: 24, backgroundColor: .custom.secondaryOrange, personImage: Image(.person), imagePersonColor: .custom.textWhite)
+                HStack(spacing: 0) {
+                    Text(helloText)
+                    Text(profileName)
+                }
+                .foregroundStyle(Color.custom.textWhite)
+                .font(.custom(.semiBold, size: 16))
+                
+                Spacer()
+                
+                NavigationLink {
+                    // Переход на экран избранных филмов
+                    EmptyView()
+                } label: {
+                    LikeHeartView(heartFrameCornerRadius: 12, heartFrameSize: 32, heartImageSize: 24, heartImageColor: .custom.secondaryRed, backgroundColor: .custom.primarySoft, heartImage: Image(.heart))
+                }
+            }
+        }
+    }
 }
